e5dcdabe93eb51dd9b2edae1dab10ba75c4794c5
Nguyễn Chiến <66434938+NgChien260399@users.noreply.github.com>
2020-10-12 21:40:03 +0700

    12/10/2020

--- DAL/Interfaces/IProductRepository.cs
+++ DAL/Interfaces/IProductRepository.cs
@@ -12,5 +12,6 @@ namespace DAL
         List<ProductModel> GetDataAll();
         List<ProductModel> GetDataNew();
         List<ProductModel> Search(int pageIndex, int pageSize, out long total, string category_id);
+        List<ProductModel> Search1(int pageIndex, int pageSize, out long total, string brand_id);
     }
 }
--- Model/CategoryModel.cs
+++ Model/CategoryModel.cs
@@ -8,8 +8,8 @@ namespace Model
         public string parent_category_id { get; set; }
         public string category_id { get; set; }
         public string category_name { get; set; }
-        public string seq_num { get; set; }
-        public short? url { get; set; }
+        public string url { get; set; }
+        public short? seq_num { get; set; }
         public List<CategoryModel> children { get; set; }
         public string type { get; set; }
     }
