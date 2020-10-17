8f61eed0afd7c36d90612aa173cb19c2e5dc3d53
Nguyễn Chiến <66434938+NgChien260399@users.noreply.github.com>
2020-10-17 15:28:47 +0700

    17/10/2020

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -129,7 +129,7 @@ namespace API.Controllers
                 string hoten = "";
                 if (formData.Keys.Contains("hoten") && !string.IsNullOrEmpty(Convert.ToString(formData["hoten"]))) { hoten = Convert.ToString(formData["hoten"]); }
                 string taikhoan = "";
-                if (formData.Keys.Contains("taikhoan") && !string.IsNullOrEmpty(Convert.ToString(formData["taikhoan"]))) { hoten = Convert.ToString(formData["taikhoan"]); }
+                if (formData.Keys.Contains("taikhoan") && !string.IsNullOrEmpty(Convert.ToString(formData["taikhoan"]))) { taikhoan = Convert.ToString(formData["taikhoan"]); }
                 long total = 0;
                 var data = _userBusiness.Search(page, pageSize, out total, hoten, taikhoan);
                 response.TotalItems = total;
